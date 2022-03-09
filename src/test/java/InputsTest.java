f8d20bfac2a5c10761f2ef27ba221cdec3159614
Marina <Zanko98@mail.ru>
2022-03-09 19:19:14 +0300

    dited InputsTest

--- src/test/java/InputsTest.java
+++ src/test/java/InputsTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -9,20 +10,21 @@ public class InputsTest extends Basetest {
     @Test
     public void inputs() {
         driver.get("http://the-internet.herokuapp.com/inputs");
-        driver.findElement(By.tagName("input")).sendKeys("10");
-        driver.findElement(By.tagName("input")).sendKeys(Keys.ARROW_UP);
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "11");
-        driver.findElement(By.tagName("input")).sendKeys(Keys.ARROW_DOWN);
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "10");
-        driver.findElement(By.tagName("input")).sendKeys(Keys.BACK_SPACE);
-        driver.findElement(By.tagName("input")).sendKeys("1");
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "11");
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "11");
-        driver.findElement(By.tagName("input")).sendKeys("tvkjhgv");
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "11");
-        driver.findElement(By.tagName("input")).sendKeys(Keys.ARROW_LEFT);
-        driver.findElement(By.tagName("input")).sendKeys(Keys.BACK_SPACE);
-        driver.findElement(By.tagName("input")).sendKeys("-");
-        assertEquals(driver.findElement(By.tagName("input")).getAttribute("value"), "-1");
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("10");
+        input.sendKeys(Keys.ARROW_UP);
+        assertEquals(input.getAttribute("value"), "11");
+        input.sendKeys(Keys.ARROW_DOWN);
+        assertEquals(input.getAttribute("value"), "10");
+        input.sendKeys(Keys.BACK_SPACE);
+        input.sendKeys("1");
+        assertEquals(input.getAttribute("value"), "11");
+        assertEquals(input.getAttribute("value"), "11");
+        input.sendKeys("tvkjhgv");
+        assertEquals(input.getAttribute("value"), "11");
+        input.sendKeys(Keys.ARROW_LEFT);
+        input.sendKeys(Keys.BACK_SPACE);
+        input.sendKeys("-");
+        assertEquals(input.getAttribute("value"), "-1");
     }
 }
